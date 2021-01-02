3281fd3b4f89b41249b487d7e1c76348e5a1b869
Jonathan Bisson <research@bjonnh.net>
2021-01-01 20:55:19 -0600

    [Quality] Reordering.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
-        val kotlinterVersion: String by settings
         val versionsPluginVersion: String by settings
         val detektVersion: String by settings
         val ktlintVersion: String by settings
+        val kotlinterVersion: String by settings
 
         kotlin("jvm") version kotlinVersion
         id("com.github.ben-manes.versions") version versionsPluginVersion
